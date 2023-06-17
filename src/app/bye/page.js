17dd8fe8fcbe89f9026a740bd91e243c64f5e12d
PharmingApp <dagreatpumpkin2@gmail.com>
2023-06-18 00:51:26 +0530

    base of login

---
+++ src/app/bye/page.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 py-6">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username" />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
