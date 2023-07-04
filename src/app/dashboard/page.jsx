044c3171db765011cd90921db09efb28a3d50d5f
PharmingApp <dagreatpumpkin2@gmail.com>
2023-07-05 01:00:15 +0530

    Dashboard CSS
    jaydeep dies a painful death

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,7 +2,24 @@ import Link from "next/link";
 
 export default function Dashboard(){
     return (
-        <Link href="/table"> Table </Link>
+        <div className="w-[1920px] h-[975px] p-2.5 bg-zinc-900 flex-col justify-center items-center inline-flex">
+            <div className="self-stretch px-[29px] py-[18px] justify-end items-start gap-[147px] inline-flex">
+                <div className="px-[29px] py-[9px] bg-white rounded-[25px] justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-zinc-900 text-[18px] font-bold">Sign Out</div>
+                </div>
+            </div>
+            <div className="self-stretch grow shrink basis-0 justify-center items-center gap-[230px] inline-flex">
+                <div className="w-[300px] h-[200px] bg-zinc-700 shadow-black shadow-2xl rounded-[25px] justify-center items-center gap-2.5 flex">
+                    <Link className="text-center text-zinc-100 text-[24px] font-bold" href="/medicines">Medicines</Link>
+                </div>
+                <div className="w-[300px] h-[200px] bg-zinc-700 shadow-black shadow-2xl rounded-[25px] justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-zinc-100 text-[24px] font-bold">Invoice</div>
+                </div>
+                <div className="w-[300px] h-[200px] bg-zinc-700 shadow-black shadow-2xl rounded-[25px] justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-zinc-100 text-[24px] font-bold">Records</div>
+                </div>
+            </div>
+        </div>
 
     )
 }
